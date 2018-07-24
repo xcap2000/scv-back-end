e0723e8f0f51d5fbe2724a80e98627a2823b2130
Carlos Adriano Portes <xcap2000@outlook.com>
2018-07-24 02:03:39 +0000

    Add W3 prefixed domain to SSL

--- SCVBackend/Controllers/WellKnownController.cs
+++ SCVBackend/Controllers/WellKnownController.cs
@@ -6,9 +6,15 @@ namespace SCVBackend.Controllers
     public class WellKnownController : Controller
     {
         [Route("/.well-known/acme-challenge/1oJfhdhjvnT8wwbyJ6b8-S_7qjEcfUHHitqPeTEHlQY")]
-        public IActionResult Index()
+        public IActionResult Domain()
         {
             return File(Encoding.UTF8.GetBytes("1oJfhdhjvnT8wwbyJ6b8-S_7qjEcfUHHitqPeTEHlQY.5L4sA11CpZAIg_CZuZtDvB7wjHxTBGV5plDRWt_aurs"), "text/plain");
+        }
+
+        [Route("/.well-known/acme-challenge/c_2dZYJVQ8ush2rUdynZL57zUGTrsUgisFenTu1C390")]
+        public IActionResult DomainWithW3()
+        {
+            return File(Encoding.UTF8.GetBytes("c_2dZYJVQ8ush2rUdynZL57zUGTrsUgisFenTu1C390.5L4sA11CpZAIg_CZuZtDvB7wjHxTBGV5plDRWt_aurs"), "text/plain");
         }
     }
 }
